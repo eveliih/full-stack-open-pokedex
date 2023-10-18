f02dc2c41ec61466d712c00dab41a8d90708b2ef
Eveliina <eveliina.heino@metropolia.fi>
2023-10-18 23:23:13 +0300

    check if deployment rolls back correctly

--- app.js
+++ app.js
@@ -11,4 +11,12 @@ app.listen(PORT, () => {
   console.log('server started on port 3000')
 })
 
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
 
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
